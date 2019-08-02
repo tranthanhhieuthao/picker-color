fac0575dfeb62f69a0ab7ebd749cd3635d43e6a2
unknown <tranthanhhieuthao@gmail.com>
2019-08-03 01:06:13 +0700

    bt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorComponent } from './color/color.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
